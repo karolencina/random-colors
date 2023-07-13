edbe146e438333599839cdc9a9c2652bf3b51283
Karolina Lencina <karolencina@icloud.com>
2023-07-13 14:29:36 +0100

    Create Random Colours app

---
+++ RandomColours/ColorsDetailVC.swift
@@ -0,0 +1,32 @@
+//
+//  ColorsDetailVC.swift
+//  RandomColours
+//
+//  Created by Karolina Lencina on 11/07/2023.
+//
+
+import UIKit
+
+class ColorsDetailVC: UIViewController {
+    
+    var color: UIColor?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = color ?? UIColor.blue
+    }
+
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+
